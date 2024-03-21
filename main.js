6eefb6af74ddd0856d16f3d0c7dd7783f5f4d564
innocentemutabazi <innocentemutabazi@gmail.com>
2024-03-21 23:00:58 +0200

    added html and css and updated javascript to make it a usable webpage

--- main.js
+++ main.js
@@ -1,49 +1,31 @@
-let raceNumber = Math.floor(Math.random() *1000);
-let registeredEarly = false;
-let age = 19;
-if (age < 17 && registeredEarly === true) {
-    raceNumber += 1;
-}
-if ( age > 18 && registeredEarly === true){
-    console.log('Your race number is ' + (raceNumber += 1) + ' and you will run at 9:30 a.m');
-} else if (age > 18 && (!(registeredEarly === true))){
-   console.log('Your race number is ' + (raceNumber += 1) + ' and you will run at 11 a.m');
-}
-else if ( age < 18 ){
-    console.log('Your race number is ' + (raceNumber += 1) + ' and you will run at 12:30 p.m');
-} else {
-    console.log('Please seek the registration desk for help.');
-}
+document.getElementById('register-button').addEventListener('click', function () {
+    const ageInput = document.getElementById('age');
+    const age = parseInt(ageInput.value);
+    const registrationTime = document.getElementById('registration-time').value;
 
- 
- age = 25;
- registeredEarly = true;
- if (age < 17 && registeredEarly === true) {
-    raceNumber += 1;
-}
-if ( age > 18 && registeredEarly === true){
-    console.log('Your race number is ' + (raceNumber += 1) + ' and you will run at 9:30 a.m');
-} else if (age > 18 && (!(registeredEarly === true))){
-   console.log('Your race number is ' + (raceNumber += 1) + ' and you will run at 11 a.m');
-}
-else if ( age < 18 ){
-    console.log('Your race number is ' + (raceNumber += 1) + ' and you will run at 12:30 p.m');
-} else {
-    console.log('Please seek the registration desk for help.');
-}
+    let raceDetails = '';
 
- age = 18;
- registeredEarly = false;
- if (age < 17 && registeredEarly === true) {
-    raceNumber += 1;
-}
-if ( age > 18 && registeredEarly === true){
-    console.log('Your race number is ' + (raceNumber += 1) + ' and you will run at 9:30 a.m');
-} else if (age > 18 && (!(registeredEarly === true))){
-   console.log('Your race number is ' + (raceNumber += 1) + ' and you will run at 11 a.m');
-}
-else if ( age < 18 ){
-    console.log('Your race number is ' + (raceNumber += 1) + ' and you will run at 12:30 p.m');
-} else {
-    console.log('Please seek the registration desk for help.');
-}
+    if (ageInput.value.trim() === '') {
+        raceDetails = 'Please enter your age.';
+    } else if (age < 0 || isNaN(age)) {
+        raceDetails = 'Please enter a valid age.';
+    } else if (age < 9) {
+        raceDetails = 'Sorry, you are too young to participate.';
+    } else if (age > 80) {
+        raceDetails = 'Sorry, you are too old to participate.';
+    } else {
+        let raceNumber = Math.floor(Math.random() * 1000) + 1;
+
+        if (age < 17) {
+            raceNumber += 1000;
+        }
+
+        if (registrationTime === 'early') {
+            raceDetails = `Your race number is ${raceNumber} and you will run at 9:00 a.m.`;
+        } else if (registrationTime === 'late') {
+            raceDetails = `Your race number is ${raceNumber} and you will run at 10:30 a.m.`;
+        }
+    }
+
+    document.getElementById('race-details').textContent = raceDetails;
+});
